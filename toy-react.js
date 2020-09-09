fe3ba518b00a134bfdc13457432515180ae3a132
carl.yu <carl.yu@carlyudeMacBook-Pro.local>
2020-09-10 01:00:57 +0800

    rerender

--- toy-react.js
+++ toy-react.js
@@ -1,14 +1,36 @@
+const RENDER_TO_DOM = Symbol("render to dom")
+
 class ElementWrapper {
     constructor(type) {
         this.root = document.createElement(type)
     }
 
+    // 如果 name 以 on 开头 加上事件监听
     setAttribute(name, value) {
-        this.root.setAttribute(name, value)
+        // [\s\S] 表所有内容
+        if (name.match(/^on([\s\S]+)$/)) {
+            // 将第一个字母替换成小写
+            this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value)
+        } else {
+            // className -> class
+            if (name === 'className') {
+                this.root.setAttribute('class', value)
+            } else {
+                this.root.setAttribute(name, value)
+            }
+        }
     }
 
     appendChild(component) {
-        this.root.appendChild(component.root)
+        let range = document.createRange()
+        range.setStart(this.root, this.root.childNodes.length)
+        range.setEnd(this.root, this.root.childNodes.length)
+        component[RENDER_TO_DOM](range)
+    }
+
+    [RENDER_TO_DOM](range) {
+        range.deleteContents()
+        range.insertNode(this.root)
     }
 }
 
@@ -19,11 +41,11 @@ class TextWrapper {
     constructor(content) {
         this.root = document.createTextNode(content)
     }
-}
 
-
-export function render(component, parentElement) {
-    parentElement.appendChild(component.root)
+    [RENDER_TO_DOM](range) {
+        range.deleteContents()
+        range.insertNode(this.root)
+    }
 }
 
 
@@ -32,6 +54,7 @@ export class Component {
         this.props = Object.create(null)
         this.children = []
         this._root = null
+        this._range = null
     }
 
     setAttribute(name, value) {
@@ -42,12 +65,47 @@ export class Component {
         this.children.push(component)
     }
 
-    // 真实的渲染过程
-    get root() {
-        if (!this._root) {
-            this._root = this.render().root
+    [RENDER_TO_DOM](range) {
+        this._range = range
+        this.render()[RENDER_TO_DOM](range);
+    }
+
+    rerender() {
+        // 先删后插入会引起bug，没有范围的 range 自动
+
+        let oldRange = this._range
+
+        // 创建一个新的 range，放在 oldRange 的start处，插入
+        let range = document.createRange()
+        range.setStart(oldRange.startContainer, oldRange.startOffset)
+        range.setEnd(oldRange.startContainer, oldRange.startOffset)
+        this[RENDER_TO_DOM](range)
+
+        // 把oldRange 的start挪到end处，再删除
+        oldRange.setStart(range.endContainer, range.endOffset)
+        oldRange.deleteContents()
+    }
+    setState(newState) {
+        // state 不存在 直接替换
+        if (this.state === null || typeof this.state !== 'object') {
+            this.state = newState
+            this.rerender()
+            return
         }
-        return this._root
+
+        let merge = (oldState, newState) => {
+            for (let p in newState) {
+                // 如果属性值是null 就直接赋值
+                if(oldState[p] === null || typeof oldState[p] !== 'object') {
+                    oldState[p] = newState[p]
+                } else {
+                    merge(oldState[p], newState[p])
+                }
+            }
+
+        }
+        merge(this.state, newState)
+        this.rerender()
     }
 }
 
@@ -74,6 +132,11 @@ export function createElement(type, attr, ...children) {
             if (typeof child === 'string') {
                 child = new TextWrapper(child)
             }
+
+            if (child === null) {
+                continue
+            }
+
             if (Array.isArray(child)) {
                 insertChild(child)
             } else {
@@ -86,3 +149,11 @@ export function createElement(type, attr, ...children) {
     return e
 }
 
+
+export function render(component, parentElement) {
+    let range = document.createRange()
+    range.setStart(parentElement, 0)
+    range.setEnd(parentElement, parentElement.childNodes.length)
+    range.deleteContents()
+    component[RENDER_TO_DOM](range)
+}
